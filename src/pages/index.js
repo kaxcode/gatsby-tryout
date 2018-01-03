22427f206ba1b8393258e24b534fb017efa07333
Kenia Valladarez <keniavalladarez@Kenia-Valladarezs-MBP.local>
2018-01-03 17:25:19 -0500

    Using gatsby-link I linked to new page-2.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Link from "gatsby-link";
 
-export default () => (
+export default () =>
   <div style={{ color: `tomato` }}>
     <h1>Hello Gatsby!</h1>
     <p>What a world.</p>
     <img src="http://lorempixel.com/400/200/" alt="" />
+    <br />
+    <div>
+      <Link to="/page-2/">Link</Link>
+    </div>
   </div>
-);
