b184eb8b3f0a8acda0cac54555c7d7d8a33f9493
Kenia Valladarez <keniavalladarez@Kenia-Valladarezs-MBP.local>
2018-01-03 17:21:42 -0500

    Adds basic header, paragraph, and image.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
-import React from "react"
+import React from "react";
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div style={{ color: `tomato` }}>
+    <h1>Hello Gatsby!</h1>
+    <p>What a world.</p>
+    <img src="http://lorempixel.com/400/200/" alt="" />
+  </div>
+);
